e0964150ba0e0a2af6ea8b5e5a9e9a1fb42c1c45
Katherine Kim <katherinekim88@gmail.com>
2020-06-03 14:05:52 -0500

    customized buttons

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function ButtonPad() {
-    return (
-        <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
-        </div>
-    )
+  return (
+    <div>
+      <button>+1</button>
+      <button>-1</button>
+      <button>Reset</button>
+    </div>
+  );
 }
